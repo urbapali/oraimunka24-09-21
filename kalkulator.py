bc6e6f83cd8afc61c85bb4fe43b846a9c6c6bd17
urbapali <urbapali@gmail.com>
2024-09-21 18:16:26 +0200

    Husvet

--- kalkulator.py
+++ kalkulator.py
@@ -1,29 +1,36 @@
-def husvet(ev):
-    a = ev % 19
-    b = ev % 4
-    c = ev % 7
-    m, n = 0, 0
-
-    if (1583 <= ev and ev <= 1699):
+def husvet(ev: int):
+    if (1583 <= ev) and (ev <= 1699):
+        szamol(ev, 22, 2)
         m = 22
         n = 2
     elif ev <= 1799:
+        szamol(ev, 23, 3)
         m = 23
         n = 3
     elif ev <= 1899:
+        szamol(ev, 23, 4)
         m = 23
         n = 4
     elif ev <= 2099:
+        szamol(ev, 24, 5)
         m = 24
         n = 5
     elif ev <= 2199:
+        szamol(ev, 24, 6)
         m = 24
         n = 6
     elif ev <= 2299:
+        szamol(ev, 25, 0)
         m = 25
         n = 0
 
-    if (1900 <= ev and ev <= 2099):
+
+def szamol(ev: int, m: int, n: int):
+    a = ev % 19
+    b = ev % 4
+    c = ev % 7
+
+    if (1900 <= ev) and (ev <= 2099):
         d = ((19 * a) + m) % 30
         e = ((2 * b) + (4 * c) + (6 * d) + n) % 7
 
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import kalkulator
 
 ev = int(input("Adj meg egy évszámot 1900-2099 között: \n"))
-
 kalkulator.husvet(ev)
-
